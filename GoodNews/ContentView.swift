58c1b707c26b04687b19b2b95379a88f086bf8cc
far1h <farihmuhammad45@gmail.com>
2025-06-18 08:57:00 +0200

    Initial Commit

---
+++ GoodNews/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GoodNews
+//
+//  Created by Farih Muhammad on 18/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoodNews/GoodNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodNewsApp.swift
+//  GoodNews
+//
+//  Created by Farih Muhammad on 18/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct GoodNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
